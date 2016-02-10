ce6903d14c1d0c9cc926b9eee2eeb65fe3f2c5ea
Mark Ryan <markryan@marks-macbook-pro.local>
2016-02-10 15:19:03 +0000

    Initial Commit

---
+++ Pitch PerfectTests/Pitch_PerfectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pitch_PerfectTests.swift
+//  Pitch PerfectTests
+//
+//  Created by Mark Ryan on 10/02/2016.
+//  Copyright © 2016 Mark Ryan. All rights reserved.
+//
+
+import XCTest
+@testable import Pitch_Perfect
+
+class Pitch_PerfectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
